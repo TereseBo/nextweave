44bf0736b7efb756fd2368946a9cf2c3cabce31d
TereseBo <terese.boderus@gmail.com>
2024-03-15 11:58:08 +0100

    Styling of signin and up

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,10 @@
+//Page for sign-in, only containing content required for signin provided by clerk
 import '@/app/(auth)/auth.scss'
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex justify-center py-24">
+    <div className='signin_container'>
       <SignIn />
     </div>
   );
