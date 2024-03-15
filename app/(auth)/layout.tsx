22d40c637303a904e891ff5e276e882a0b75dad2
TereseBo <terese.boderus@gmail.com>
2024-03-15 11:54:09 +0100

    Added auth lauout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+
+import { Header } from '@/app/components/zSharedComponents/Header'
+
+export default function AuthBaseLayout({
+    children,
+}: {
+    children: React.ReactElement
+}) {
+    return (
+        <>
+            <main id='auth'>
+            <Header title="Welcome to the weaving project creator!" text="A registered account will give you access to more features, but some functionality is also available without sign in. "></Header>
+                {children}
+            </main>
+        </>
+    )
+}
