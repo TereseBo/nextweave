06c3ee49df65ca85cc9bf39e73b0415623923a75
TereseBo <terese.boderus@gmail.com>
2024-04-09 11:23:40 +0200

    Added links after restructure

--- app/components/(userpages)/profile/aside.tsx
+++ app/components/(userpages)/profile/aside.tsx
@@ -7,9 +7,9 @@ export function Aside() {
     return (
 
         <nav className="side-bar">
-            <Link href="/(userpages)/profile">Drafts</Link>
-            <Link href="/(userpages)/looms">Looms</Link>
-            <Link href="/(userpages)/reeds">Reeds</Link>
+            <Link href="/(userpages)/profile/drafts">Drafts</Link>
+            <Link href="/(userpages)/profile/looms">Looms</Link>
+            <Link href="/(userpages)/profile/reeds">Reeds</Link>
         </nav>
     )
 }
