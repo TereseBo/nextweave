5eb73ea359867b82352bb2dae2666e734ab62a94
TereseBo <terese.boderus@gmail.com>
2024-04-18 08:29:24 +0200

    Added delete draft functionality

--- app/api/[userId]/draft/[id]/route.ts
+++ app/api/[userId]/draft/[id]/route.ts
@@ -1,5 +1,4 @@
-
-//Route for actions on single weaves as specified by their id
+//Route for actions on single weaves owned by a user as specified by user and weave Id
 import { Db } from 'mongodb'
 import { ObjectId } from 'mongodb'
 import { NextResponse } from 'next/server'
@@ -8,19 +7,18 @@ import { dbConnection } from '@/app/resources/db/mongodb'
 
 export async function GET(
     req: Request,
-    { params }: { params: { userId: string, id:string } }) {
+    { params }: { params: { userId: string, id: string } }) {
     //Fetches one weave for the user by _id
-    const {userId, id}  = params
+    const { userId, id } = params
 
-    const _id= new ObjectId(id)
+    const _id = new ObjectId(id)
     if (!userId) {
         return new NextResponse('Unauthorized', { status: 401 });
     }
 
     try {
         const db = await dbConnection() as Db
-        let dbResponse = await db.collection('drafts').findOne({_id, userId})
-        console.log(dbResponse)
+        let dbResponse = await db.collection('drafts').findOne({ _id, userId })
 
         if (!dbResponse) {
             return new NextResponse(null, { status: 204 });
@@ -39,30 +37,33 @@ export async function GET(
     }
 }
 
-
 export async function DELETE(
     req: Request,
-    { params }: { params: { userId: string, id:string } }) {
-    //Fetches one weave for the user by id
-    const {userId, id}  = params
+    { params }: { params: { userId: string, id: string } }) {
+    //Deletes one weave for the user by id
+    const { userId, id } = params
 
-    const _id= new ObjectId(id)
+    const _id = new ObjectId(id)
     if (!userId) {
         return new NextResponse('Unauthorized', { status: 401 });
     }
 
     try {
         const db = await dbConnection() as Db
-        let dbResponse = await db.collection('drafts').findOne({ _id , userId })
+        let dbResponse = await db.collection('drafts').deleteOne({ _id, userId })
+        console.log(dbResponse)
+        //
+        //
+
+        console.log('Response in patch toute')
         console.log(dbResponse)
 
-        if (!dbResponse) {
-            return new NextResponse('You have no saved weaves', { status: 204 });
+        if (dbResponse.deletedCount !== 1) {
+            return new NextResponse('No weave to update found', { status: 204 });
+        } else {
+
+            return NextResponse.json('Weave updated', { status: 200 });
         }
-
-        let draft = dbResponse.weave
-
-        return NextResponse.json({ weaveObject: draft }, { status: 200 });
 
     } catch (error) {
         console.log('api/draft/GET', error);
@@ -75,11 +76,11 @@ export async function DELETE(
 
 export async function PATCH(
     req: Request,
-    { params }: { params: { userId: string, id:string } }) {
+    { params }: { params: { userId: string, id: string } }) {
     //Updates one weave for the user
-    const {userId, id}  = params
+    const { userId, id } = params
 
-    const _id= new ObjectId(id)
+    const _id = new ObjectId(id)
     console.log(_id)
     if (!userId) {
         return new NextResponse('Unauthorized', { status: 401 });
@@ -87,20 +88,19 @@ export async function PATCH(
 
     try {
 
-            const db = await dbConnection() as Db
-            const body = await req.json();
-            const { weaveObject, publicStatus } = body.values
-            let updates = { weave: weaveObject, updated: Date.now(), public: publicStatus }
-        
-        let dbResponse = await db.collection('drafts').updateOne({ _id , userId },{$set:{weave: weaveObject, updated: Date.now(), public: publicStatus}})
+        const db = await dbConnection() as Db
+        const body = await req.json();
+        const { weaveObject, publicStatus } = body.values
+       
+        let dbResponse = await db.collection('drafts').updateOne({ _id, userId }, { $set: { weave: weaveObject, updated: Date.now(), public: publicStatus } })
         console.log('Response in patch toute')
         console.log(dbResponse)
 
-        if (dbResponse.modifiedCount!==1) {
+        if (dbResponse.modifiedCount !== 1) {
             return new NextResponse('No weave to update found', { status: 204 });
-        }else{
+        } else {
 
-        return NextResponse.json( 'Weave updated', { status: 200 });
+            return NextResponse.json('Weave updated', { status: 200 });
         }
 
     } catch (error) {
--- app/components/(userpages)/profile/drafts/EditDraftForm.tsx
+++ app/components/(userpages)/profile/drafts/EditDraftForm.tsx
@@ -7,7 +7,7 @@ import { useState } from 'react'
 import { StateDraft } from '@/app/components/draft/draft/StateDraft'
 import { useUserContext } from '@/app/resources/contexts/usercontext'
 
-export function EditDraftForm(params: { resource: any, open: boolean}) {
+export function EditDraftForm(params: { resource: any, open: boolean }) {
 
     const { open, resource } = params
     const draftId = resource._id
@@ -15,7 +15,7 @@ export function EditDraftForm(params: { resource: any, open: boolean}) {
     //TODO:Move colorpixker style to relevant place
 
     const [updatedWeaveObj, setUpdatedWeaveObj] = useState<WeaveObject>(JSON.parse(JSON.stringify({ ...resource.weave })))
-    const updateObj=(neObj:WeaveObject)=>{setUpdatedWeaveObj(neObj)}
+    const updateObj = (neObj: WeaveObject) => { setUpdatedWeaveObj(neObj) }
 
     //TODO: replace user with acctual id
     async function editDraft(e: any) {
@@ -41,10 +41,19 @@ export function EditDraftForm(params: { resource: any, open: boolean}) {
     }
 
     function deleteDraft(e: any) {
-        //TODO: Add functionality
-      
+
+        fetch(`/api/${user}/draft/${draftId}`, { method: 'DELETE' })
+        .then(function (response) {
+
+            if (response.status == 200) {
+                //TODO:Update draft in usderContext to match
+                alert('Draft deleted!')
+            } else {
+                alert('Ops, could not delete the draft')
+            }
+        })
     }
-    
+
     return (
         <div className={params.open ? 'edit-draft-container' : 'hidden'}>
             <div className='edit-draft'>
