345aa6af3c403bcc1c44411708445c2d3fd20576
TereseBo <terese.boderus@gmail.com>
2024-04-18 09:09:53 +0200

    Finalized new draft in userpage

--- app/components/(userpages)/profile/drafts/AddDraft.tsx
+++ app/components/(userpages)/profile/drafts/AddDraft.tsx
@@ -1,12 +1,14 @@
 'use client'
 import './adddraft.scss'
 
-import { useState} from 'react'
+import { useState } from 'react'
 
+import { ColorPicker } from '@/app/components/draft/colorpicker/Colorpicker'
+import { Draft } from '@/app/components/draft/draft/Draft'
 import { DbSaveWeave } from '@/app/components/draft/draftoptions/dbhandler/DbSaveWeave'
 import { SecondaryMenu } from '@/app/components/zSharedComponents/SecondaryMeny'
 
-import { EditDraftForm } from './EditDraftForm'
+import { DisplayCard } from '../../DisplayCard'
 
 export function AddDraft() {
 
@@ -16,11 +18,16 @@ export function AddDraft() {
 
     return (
         <>
-            <div className={open ? 'new-draft-container' : 'hidden'} onMouseLeave={closeForm}>
-                <EditDraftForm open={true} resource={'none'} closeForm={closeForm}/>
-                <SecondaryMenu>
-                    <DbSaveWeave />
-                </SecondaryMenu>
+            <div className={open ? 'buffer' : 'hidden'} onMouseLeave={closeForm}>
+                <DisplayCard>
+                    <div className='new-draft-container'>
+                        <ColorPicker />
+                        <Draft />
+                        <SecondaryMenu>
+                            <DbSaveWeave />
+                        </SecondaryMenu>
+                    </div>
+                </DisplayCard>
             </div>
             <button onClick={openForm}>Create new draft</button>
         </>
