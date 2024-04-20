32cdf394ce64ea1339cada3c9f503518a1c3acbe
TereseBo <terese.boderus@gmail.com>
2024-04-20 22:53:24 +0200

    Added constant for reeds

--- app/resources/constants/weaveDefaults.ts
+++ app/resources/constants/weaveDefaults.ts
@@ -6,5 +6,17 @@ const defaultColor='#000000'
 const defaultWeftColor='#0313fc'
 const defaultWarpColor='#878787'
 
-export {defaultColor,defaultDraftHeight, defaultDraftWidth, defaultShafts, defaultTreadles, defaultWarpColor, defaultWeftColor}
+
+//Reed defaults
+const reedUnits:ReedUnit[]=['cm','in']
+
+const templateReed: Reed = {
+    id: undefined,
+    section: 10,
+    dents: 50,
+    unit: reedUnits[0],
+    length: 100
+}
+
+export {defaultColor,defaultDraftHeight, defaultDraftWidth, defaultShafts, defaultTreadles, defaultWarpColor, defaultWeftColor, reedUnits, templateReed}
 
