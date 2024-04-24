df66e504cdaf6f2f82ae8631bb9788848071ef37
TereseBo <terese.boderus@gmail.com>
2024-04-24 09:08:51 +0200

    Updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/weaver/:path*','/'],
+  publicRoutes: ['/weaver/calculator','/weaver/draft'],
 });
 
 export const config = {
