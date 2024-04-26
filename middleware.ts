942aa0ae5e64af2ab69d3373400aa2eb7028a4ad
TereseBo <terese.boderus@gmail.com>
2024-04-26 16:28:41 +0200

    Change of protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/:path*','/'],
+  publicRoutes: ['/weaver/:path*','/weaver'],
 });
 
 export const config = {
