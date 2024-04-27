35d01886b3eb28e32eb15fe9a1e02ad896c13d1a
TereseBo <terese.boderus@gmail.com>
2024-04-27 14:47:18 +0200

    Replaced components in user menu with clerk versions

--- app/components/zSharedComponents/UserMenu.tsx
+++ app/components/zSharedComponents/UserMenu.tsx
@@ -2,7 +2,7 @@
 //Contains an optional nav-menu for signed in users, otherwise a link to sign in
 import './userMenu.scss'
 
-import { UserButton } from '@clerk/nextjs'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import { useAuth } from '@clerk/nextjs'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
@@ -13,10 +13,15 @@ export function UserMenu() {
 
     return (
         <div className='usermenu-container'>
-            
-            {isSignedIn ?  <Link className="icon" href={'/profile'}>Profile</Link>: (pathname !== '/sign-in' ? <Link className="icon" href="/sign-in">Sign in</Link> : null)}
-            
-            < UserButton afterSignOutUrl='/weaver' />
+
+            {/* {isSignedIn ?  <Link className="icon" href={'/profile'}>Profile</Link>: (pathname !== '/sign-in' ? <Link className="icon" href="/sign-in">Sign in</Link> : null)} */}
+            <SignedOut>
+                {pathname !== '/sign-in' ? <Link className="icon" href="/sign-in">Sign in</Link> : null}
+            </SignedOut>
+            <SignedIn>
+                <Link className="icon" href={'/profile'}>Profile</Link>< UserButton afterSignOutUrl='/weaver' />
+            </SignedIn>
+
         </div>
     )
 }
