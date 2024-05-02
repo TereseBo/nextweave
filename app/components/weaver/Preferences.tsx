43ae63fcdbee4bbdac112a08cda0fbb1ce60845b
TereseBo <terese.boderus@gmail.com>
2024-05-02 09:04:21 +0200

    Adding fail-safes for min gridsize

--- app/components/weaver/Preferences.tsx
+++ app/components/weaver/Preferences.tsx
@@ -11,12 +11,13 @@ export function Preferences() {
     const { treadles, setTreadles, shafts, setShafts } = useWeaveContext()
     const router = useRouter()
 
-    //Validation of the content in editedLoomState, is to be used before submission of data to DB
-    function validateFormData(value:number) {
+    //Validation of the content in in staate before generating a grid in draft
+    function validateStateData() {
         let message = ''
 
-        if (value < lowerAccessoryGridLimit || value > upperAccessoryGridLimit ) { message = `Please enter a number of shafts and treadles between ${lowerAccessoryGridLimit} and ${upperAccessoryGridLimit}.` }
-        
+        if (treadles < lowerAccessoryGridLimit || treadles > upperAccessoryGridLimit) { message = `Please enter a number of shafts and treadles between ${lowerAccessoryGridLimit} and ${upperAccessoryGridLimit}.` }
+        if (shafts < lowerAccessoryGridLimit || shafts > upperAccessoryGridLimit) { message = `Please enter a number of shafts and treadles between ${lowerAccessoryGridLimit} and ${upperAccessoryGridLimit}.` }
+
         if (message === '') {
             return true
         } else {
@@ -33,8 +34,10 @@ export function Preferences() {
     }
     function handleSubmit(e: React.FormEvent) {
 
-        console.log(e)
         e.preventDefault()
+
+        if (!validateStateData()) return
+
         router.push('/weaver/draft')
     }
 
@@ -44,11 +47,11 @@ export function Preferences() {
                 <h3 className="form-header">Loom information</h3>
                 <div>
                     <label htmlFor="shafts">Number of shafts</label>
-                    <input type="number" name="shafts" id="shafts" min={lowerAccessoryGridLimit} max={upperAccessoryGridLimit} placeholder={shafts.toString()} onChange={(e) => { updateShaft(e) }} />
+                    <input type="number" name="shafts" id="shafts" min={lowerAccessoryGridLimit} max={upperAccessoryGridLimit} placeholder={shafts.toString()} onChange={(e) => { updateShaft(e) }} required={true} />
                 </div>
                 <div>
                     <label htmlFor="thredles">Number of thredles</label>
-                    <input type="number" name="thredles" id="thredles" min={lowerAccessoryGridLimit} max={upperAccessoryGridLimit} placeholder={treadles.toString()} onChange={(e) => { updateTreadle(e) }} />
+                    <input type="number" name="thredles" id="thredles" min={lowerAccessoryGridLimit} max={upperAccessoryGridLimit} placeholder={treadles.toString()} onChange={(e) => { updateTreadle(e) }} required={true} />
                 </div>
                 <input type='submit' value="Submit" />
             </form>
